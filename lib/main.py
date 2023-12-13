7d4263af3fc92a307591c594f8a28fd0ad437e1b
jim <jim.otieno@student.moringaschool.com>
2023-12-13 15:25:04 +0300

    table insertion and instance testing

--- lib/main.py
+++ lib/main.py
@@ -1,9 +1,54 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.sql import func
-import random
 
 engine = create_engine("sqlite:///book.db")
-
 Base = declarative_base()
+
+class Book(Base):
+    __tablename__='books'
+    id=Column(Integer(),primary_key=True)
+    title=Column(String())
+    author=Column(String())
+    isbn=Column(Integer())
+    publication_date=Column(String())
+    description=Column(String())
+    publisher=Column(String())
+    language=Column(String())
+    pages_count=Column(Integer())
+    rating=Column(Integer())
+
+    def __repr__(self):
+        return f'Book(id={self.id},' + \
+               f'title={self.title},' + \
+               f'author={self.author},' + \
+               f'isbn={self.isbn},' + \
+               f'publication_date={self.publication_date},' + \
+               f'description={self.description},' + \
+               f'publisher={self.publisher},' + \
+               f'language={self.language},' + \
+               f'pages_count={self.pages_count},' + \
+               f'rating={self.rating})'
+
+class Author(Base):
+    __tablename__='authors'
+    
+    id=Column(Integer(),primary_key=True)
+    name=Column(String())
+    best_seller=Column(String())
+    
+    def __repr__(self):
+        return f'Author(id={self.id},' + \
+               f'name={self.name},' + \
+               f'best_seller={self.best_seller})'
+
+class Genrer(Base):
+    __tablename__='genrers'
+    id=Column(Integer(),primary_key=True)
+    name=Column(String())
+
+    def __repr__(self):
+        return f'Genrer(id={self.id},' + \
+               f'name={self.name})'
+
+# Create tables in the database
+Base.metadata.create_all(engine)
---
+++ lib/seed.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from main import Book,Author,Genrer
+
+if __name__ == '__main__':
+    engine=create_engine("sqlite:///book.db")
+    Session=sessionmaker(bind=engine)
+    session=Session()
+
+    # session.query(Book).delete()
+    # session.query(Author).delete()
+    # session.query(Genrer).delete()
+    
+    book1 = Book(title='The Shadow of the Wind', author='Carlos Ruiz Zafón', isbn=9780143034902, publication_date='2004-04-12', description='A mesmerizing literary thriller.', publisher='Penguin Books', language='English', pages_count=487, rating=4)
+    book2 = Book(title='1984', author='George Orwell', isbn=9780451524935, publication_date='1949-06-08', description='A dystopian novel.', publisher='Signet Classic', language='English', pages_count=328, rating=5)
+    book3 = Book(title='To Kill a Mockingbird', author='Harper Lee', isbn=9780061120084, publication_date='1960-07-11', description='A classic of modern American literature.', publisher='Harper Perennial Modern Classics', language='English', pages_count=336, rating=4)
+    book4 = Book(title='The Great Gatsby', author='F. Scott Fitzgerald', isbn=9780743273565, publication_date='1925-04-10', description='An exploration of decadence and idealism.', publisher='Scribner', language='English', pages_count=180, rating=5)
+    book5 = Book(title='Pride and Prejudice', author='Jane Austen', isbn=9780141439518, publication_date='1813-01-28', description='A romantic novel of manners.', publisher='Jungle',language='japanese',pages_count=200,rating=7)
+
+    author1 = Author(name='Carlos Ruiz Zafón', best_seller='The Shadow of the Wind')
+    author2 = Author(name='George Orwell', best_seller='1984')
+    author3 = Author(name='Harper Lee', best_seller='To Kill a Mockingbird')
+    author4 = Author(name='F. Scott Fitzgerald', best_seller='The Great Gatsby')
+    author5 = Author(name='Jane Austen', best_seller='Pride and Prejudice')
+    
+    genrer1 = Genrer(name='Fantasy')
+    genrer2 = Genrer(name='Mystery')
+    genrer3 = Genrer(name='Romance')
+    genrer4 = Genrer(name='Science Fiction')
+    genrer5 = Genrer(name='Thriller')
+
+    session.add_all([author1, author2, author3, author4, author5, book1, book2, book3, book4, book5, genrer1, genrer2, genrer3, genrer4, genrer5])
+    session.commit()
